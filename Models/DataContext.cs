925e99cef939ff9005d54e609dba1507b268bea6
Jason Davila <jsndavila1@gmail.com>
2019-12-26 13:06:54 -0500

    In the process of changing up the DBContext in order to get user authentication working. Not complete.

--- Models/DataContext.cs
+++ Models/DataContext.cs
@@ -1,13 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebTattoo.Models;
 using WebTattoo.Models.Tables;
 
 namespace WebTattoo.Controllers
 {
-    public class DataContext:DbContext
+    public class DataContext:IdentityDbContext<AppUser, AppRole, int>
     {
         public DataContext(DbContextOptions<DataContext> options):base(options)
         {
